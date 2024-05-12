d6187990a519fd71199ba5a0801b2322c60e7e25
Tomer Klein <tomer.klein@gmail.com>
2024-05-12 11:51:14 +0300

    Fix threading issues

--- app/sqliteconnector.py
+++ app/sqliteconnector.py
@@ -9,7 +9,7 @@ class SqliteConnector:
         
     def open_connection(self):
         try:
-            self.conn = sqlite3.connect(self.db_file)
+            self.conn = sqlite3.connect(self.db_file, check_same_thread=False)
         except Error as e:
             logger.error(str(e))
 
