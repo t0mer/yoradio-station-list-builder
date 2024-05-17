1ec0176df1897309420fd63b1276ae31b70ba4fe
Tomer Klein <tomer.klein@gmail.com>
2024-05-17 23:50:19 +0300

    Adding functunality

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var stationsTable = $('#stations-by-countries').DataTable({
         "searching": true,
         "showing": false,
-        "bLengthChange": true,
+        "bLengthChange": false,
         "paging": true
     });
 
@@ -77,6 +77,35 @@ function stopPlayback() {
 }
 
 
+
+function createAddHandler(title, url) {
+    return function() {
+        addToNewList(title, url);
+    };
+}
+
+function createRemoveHandler(title, url) {
+    return function() {
+        removeFromNewList(title, url);
+    };
+}
+
+
+var newList = [];
+
+function addToNewList(title, url) {
+    newList.push({title: title, url: url, Ovol: 0});
+    console.log("Added to new list:", title, url);
+    console.log("Updated newList:", newList);
+
+}
+
+function removeFromNewList(title, url) {
+    newList = newList.filter(item => item.title !== title || item.url !== url);
+    console.log("Removed from new list:", title, url);
+    console.log("Updated newList:", newList);
+}
+
 function get_stations_by_country(country_id, dataTable) {
     dataTable.clear().draw(); // Clear existing DataTable
 
@@ -91,14 +120,16 @@ function get_stations_by_country(country_id, dataTable) {
                 var truncatedUrl = item["final_url"].length > maxLength ? item["final_url"].substring(0, maxLength) + '...' : item["final_url"];
 
                 var $urlLink = $('<a>').attr('href', item["final_url"]).text(truncatedUrl);
-                var $playButton = $('<button>').addClass('btn btn-play').html('<i class="fas fa-play"></i>').click(createPlayHandler(item["final_url"]));
-                var $stopButton = $('<button>').addClass('btn btn-stop').html('<i class="fas fa-stop"></i>').click(stopPlayback);
+                var $playButton = $('<button>').addClass('btn btn-play').html('<i class="fas fa-play" title="Play"></i>').click(createPlayHandler(item["final_url"]));
+                var $stopButton = $('<button>').addClass('btn btn-stop').html('<i class="fas fa-stop" title="Stop"></i>').click(stopPlayback);
+                var $addButton = $('<button>').addClass('btn btn-add').html('<i class="fas fa-plus" title="Add"></i>').click(createAddHandler(item["title"], item["final_url"]));
+                var $removeButton = $('<button>').addClass('btn btn-remove').html('<i class="fas fa-minus" title="Remove"></i>').click(createRemoveHandler(item["title"], item["final_url"]));
 
                 var $tr = $('<tr>').append(
                     $('<td class="vmiddle">').text(item["country"]),
                     $('<td class="vmiddle">').text(item["title"]),
                     $('<td class="vmiddle">').append($urlLink),
-                    $('<td class="srv vmiddle" id="' + item["id"] + '" >').append($playButton, ' ', $stopButton)
+                    $('<td class="srv vmiddle" id="' + item["id"] + '" >').append($playButton, ' ', $stopButton, ' ', $addButton, ' ', $removeButton)
                 );
 
                 dataTable.row.add($tr); // Add the row to DataTable
